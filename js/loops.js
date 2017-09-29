9d0895640f280dd759d64416a91a499fe1762c07
call-nicole <nicole.reneal@gmail.com>
2017-09-29 11:13:04 -0600

    Updated Progress
    conditional statements and arrays

--- js/loops.js
+++ js/loops.js
@@ -1,3 +1,7 @@
 var booksOfMormon = ["1 Nephi", "2 Nephi", "Jacob"];
 
 document.getElementById("p1").innerHTML = "hello World";
+
+ 
+var cars = ["Saab", "Volvo", "BMW"];
+document.getElementById("pig").innerHTML = cars;
