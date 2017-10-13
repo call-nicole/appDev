435e68d9e28f3d75f79d02f805b16dd46cbdd7e6
call-nicole <nicole.reneal@gmail.com>
2017-10-12 21:49:58 -0600

    Movie Method
    Movie Method

--- objects/objects.js
+++ objects/objects.js
@@ -1,16 +1,23 @@
- function Person(name, age, fav) {
+ function Person(name, age, fav, movie) {
                 this.name = name;
                 this.age = age;
                 this.fav = fav;
+                this.movie = movie
                 this.greeting = function() {
-                return "Hi! I\'m " + this.name + " I am " + this.age + " years old . I like " + this.fav ;
+                    return "Hi! I\'m " + this.name + " I am " + this.age + " years old . I like " + this.fav ;
+                };
+                this.shareMovie = function() {
+                    return "Oh, I almost forgot, my favorite movie is " + this.movie + "!";
                 };
             }
             
-            var person1 = new Person('Bob',  '46', 'Reading Books');
-            var person2 = new Person('Sarah', '19', 'Playing Sports');
+            var person1 = new Person('Bob',  '46', 'Reading Books', 'Casa Blanca');
+            var person2 = new Person('Sarah', '19', 'Playing Sports', 'Casa Blanca');
             
-            person1.name
-            document.getElementById("demo").innerHTML = person1.greeting()
-            person2.name
-            document.getElementById("demo1").innerHTML = person2.greeting()
+           
+            document.getElementById("demo").innerHTML = person1.greeting();
+            document.getElementById("movie").innerHTML = person1.shareMovie();
+
+            document.getElementById("demo1").innerHTML = person2.greeting();
+            document.getElementById("movie2").innerHTML = person2.shareMovie();
+            
