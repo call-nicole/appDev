94879b4550e80d7f48e9cc5d45b71c7b889f0c14
call-nicole <nicole.reneal@gmail.com>
2017-10-12 21:38:42 -0600

    External JS
    External JS working

--- objects/objects.js
+++ objects/objects.js
@@ -1,12 +1,16 @@
-var salva = person('Salva');
-salva.name;
-salva.greeting();
-
-
-
-function Person(name) {
-  this.name = name;
-  this.greeting = function() {
-    alert('Hi! I\'m ' + this.name + '.');
-  };
-}
+ function Person(name, age, fav) {
+                this.name = name;
+                this.age = age;
+                this.fav = fav;
+                this.greeting = function() {
+                return "Hi! I\'m " + this.name + " I am " + this.age + " years old . I like " + this.fav ;
+                };
+            }
+            
+            var person1 = new Person('Bob',  '46', 'Reading Books');
+            var person2 = new Person('Sarah', '19', 'Playing Sports');
+            
+            person1.name
+            document.getElementById("demo").innerHTML = person1.greeting()
+            person2.name
+            document.getElementById("demo1").innerHTML = person2.greeting()
