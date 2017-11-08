993069aabe1a4a95558884f2497e06f28f773d68
call-nicole <nicole.reneal@gmail.com>
2017-11-07 21:10:49 -0700

    Wrapped in Function
    API request wrapped in a function

--- application/app.js
+++ application/app.js
@@ -1,6 +1,6 @@
 //https://developers.themoviedb.org/3/getting-started/search-and-query-for-details THE BEST DOCUMENTATION
 //API key cfaf5a037f8e81c516eb5dbc1c98908f
-    
+ function setup(){   
 var idJSON = new XMLHttpRequest();
         var address = "https://api.themoviedb.org/3/search/movie?api_key=cfaf5a037f8e81c516eb5dbc1c98908f&query=jack+reacher";
         
@@ -38,5 +38,52 @@ var movJSON = new XMLHttpRequest();
     }
     movJSON.open("GET", address2, true);
         movJSON.send();
-    
+ }
 
+//function setup(){
+//    var y = document.forms["form1"]["movie"].value;
+//        localStorage.setItem("search", y);
+//    var mv = localStorage.getItem("search");
+//    document.getElementById("output").innerHTML=mv;
+//var idJSON = new XMLHttpRequest();
+//        
+//        var address = "https://api.themoviedb.org/3/search/movie?api_key=cfaf5a037f8e81c516eb5dbc1c98908f&query=jack+reacher";
+//        
+//        idJSON.onreadystatechange = function() {
+//            if(this.readyState === 4 && this.status === 200) {
+//                var data = JSON.parse(this.responseText);
+//               var id = data.results[0].id;
+//               localStorage.setItem('movieId', id);
+//               // document.getElementById("recipes").innerHTML = id;
+//                
+//            }
+//        }
+//        idJSON.open("GET", address, true);
+//        idJSON.send();
+//
+//var movJSON = new XMLHttpRequest();
+//    
+//    var begin="https://api.themoviedb.org/3/movie/"
+//    var mId = localStorage.getItem("movieId");
+//    //var search = "/images"
+//    var api = "?api_key=cfaf5a037f8e81c516eb5dbc1c98908f"
+//    
+//    var address2 = begin + mId + api;
+//
+//    document.getElementById("output").innerHTML = address2
+//
+//    movJSON.onreadystatechange = function(){
+//        if(this.readyState === 4 && this.status === 200) {
+//        var data2 = JSON.parse(this.responseText);
+//            var title = data2.original_title;
+//            var ove = data2.overview;
+//            document.getElementById("title").innerHTML = title;
+//            document.getElementById("overview").innerHTML=ove;
+//        }
+//    }
+//    movJSON.open("GET", address2, true);
+//        movJSON.send();
+//    
+//}
+//    
+//
